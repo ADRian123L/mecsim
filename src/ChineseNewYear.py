38b6499adeab50808baa7348fc1cfffc8e846a8b
ADRian123L <adrian163@usf.edu>
2022-12-16 17:55:14 -0500

    Added the json function.

--- src/ChineseNewYear.py
+++ src/ChineseNewYear.py
@@ -2,7 +2,25 @@
 the year.
 '''
 
-import resource
+import requests
+import json
 
 chinese_year_names = ('Rat','Ox','Tiger','Rabbit','Dragon','Snake',
                       'Horse','Goat','Monkey','Rooster','Dog','Pig')
+
+# Using an API:
+def api(run=False) -> None:
+    if (run):
+        response = requests.get("https://api.open-notify.org/this-api-doesnt-exist")
+    return None
+
+# Using Json:
+def j_file(info : list, run=False) -> json:
+    if (run):
+        file = json.dumps(info, indent=4)
+        return file
+
+if __name__ == "__main__":
+    api()
+    new_file = j_file( chinese_year_names,run=True)
+    print(new_file)
