c977f1e4933eae885281b964ba6d6ec6d2259de0
ADRian123L <adrian163@usf.edu>
2022-12-16 16:52:26 -0500

    Fixed issues

--- src/main.py
+++ src/main.py
@@ -7,30 +7,33 @@ from math import pi, sin, cos
 def main(inputs : dict) -> dict:
     """The function returns a dictionary with the 
     response to the inputs."""
-    # Inputs:
-    radian : float = ((pi / 180) * (inputs['angle'] + 45))
+    # Inputs:z
+    radian = ((pi / 180) * (inputs['angle'] + 45))
     # Calls the function:
-    vectors : list = vector(radian)
+    vectors = vector(radian)
     # Returns a dictionary with the outputs:
     return {
-        "angle"    : inputs['angle'],
-        "number_1" : vectors[0],
-        "number_2" : vectors[1],
-        "number_3" : vectors[2],
-        "number_4" : vectors[3]
+        "angle": inputs['angle'],
+        "number_1": vectors[0],
+        "number_2": vectors[1],
+        "number_3": vectors[2],
+        "number_4": vectors[3]
     }
 
 def vector(radian : float) -> list:
     """The function return a list with the vectors for
     controlling a vehicle."""
     # Converts to x - y components:
-    x_comp : float = cos(radian)
-    y_comp : float = sin(radian)
+    x_comp = cos(radian)
+    y_comp = sin(radian)
 # Creates a list:
-    vectors : list = [-x_comp, y_comp, -x_comp, y_comp]
-    new_vector : list = list()
+    vectors = [-x_comp, y_comp, -x_comp, y_comp]
+    new_vector = list()
     # Changes the floats into integers:
     for i in vectors[:]:
-        nums : int = round(i)
+        nums = round(i)
         new_vector.append(nums)
     return new_vector
+
+inputs = {'angle' : 90}
+print(main(inputs))
