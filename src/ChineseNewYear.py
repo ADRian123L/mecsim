77c63a58fadc93cba304c64c77085c68f8219474
ADRian123L <adrian163@usf.edu>
2022-12-16 17:31:39 -0500

    Edited the chinese new year file.

--- src/ChineseNewYear.py
+++ src/ChineseNewYear.py
@@ -1,6 +1,8 @@
 '''This program prompt the year and return the name of 
 the year.
 '''
-n = ('Rat','Ox','Tiger','Rabbit','Dragon','Snake'
-    ,'Horse','Goat','Monkey','Rooster','Dog','Pig'
-)
+
+import resource
+
+chinese_year_names = ('Rat','Ox','Tiger','Rabbit','Dragon','Snake',
+                      'Horse','Goat','Monkey','Rooster','Dog','Pig')
