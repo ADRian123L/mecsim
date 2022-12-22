64c8ef382657993c9335d4c38dcd1f275eef2243
ADRian123L <adrian163@usf.edu>
2022-12-21 20:44:42 -0500

    Added a function to return a line from a string

--- src/Chinese_Years.py
+++ src/Chinese_Years.py
@@ -1,8 +1,9 @@
 '''This program prompt the year and return the name of
 the year.
 '''
+# Import the random module:
+import random
 
-# Path: src/Chinese_Years.py
 # Name the writer:
 __author__ = "Adrian Lozada", "Jiahui Dang"
 # Name the program:
@@ -16,8 +17,9 @@ __description__ = "This program prompt the year and return the name of the year.
 # Name the license:
 __license__ = "MIT"
 
-# Write a string with information about each chinese zodiac:
-chinese_zodiac = """    
+# Facts about the Chinese zodiacs:
+# Convert each line into a string stored in a list:
+chinese_zodiac_facts = """
     The Chinese zodiac is a cycle of 12 years, with each year being represented by an animal.
     The 12 animals are: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, and Pig.
     The cycle repeats every 12 years. The years of the Chinese zodiac are determined by the Chinese calendar.
@@ -29,30 +31,45 @@ chinese_zodiac = """
     The Chinese zodiac is also used to represent people born in a particular year, month, or day.
     The Chinese zodiac is used to determine a person's personality, compatibility with others, and fortune for the year.
     The Chinese zodiac is also used to predict the luck of a person or a business.
-    The Chinese zodiac is based on a 12-year cycle, with each year in that cycle related to an animal sign.
-    The 12-year cycle is an approximation of the 11.86-year orbital period of Jupiter around the sun.
-    The 12-year cycle is also related to the 12 signs of the zodiac in Western astrology.
    """
 
-
-
-# Write a dictionary that stores the names of the years:
+# Chinese zodiacs names:
 chinese_year_names = {
                     1:'Rat', 2:'Ox', 3:'Tiger', 4:'Rabbit', 
                     5:'Dragon', 6:'Snake', 7:'Horse', 8:'Goat', 
                     9:'Monkey', 10:'Rooster', 11:'Dog', 12:'Pig'
                     }
 
+# Chinese zodiacs emojis:
+chinese_year_emojis = {
+                    1:'🐀', 2:'🐂', 3:'🐅', 4:'🐇', 
+                    5:'🐉', 6:'🐍', 7:'🐎', 8:'🐐', 
+                    9:'🐒', 10:'🐓', 11:'🐕', 12:'🐖'
+                    }   
+    
 # The function determines the name of the year:
 def chinese_zodiac(year) -> str:
     '''This function takes a year as input and returns the sign of the
      Chinese zodiac for that year.'''
+    # The function returns the sign of the Chinese zodiac for that year:
     return chinese_year_names[(year - 3) % 12]
 
+# The function returns a random line from the string:
+def random_line() -> str:
+    '''This function takes a string as input and returns a random line from the string.'''
+    random_line = random.choice(chinese_zodiac_facts.splitlines()).strip()
+    return random_line
+
 # The function driver:
-def main(chinese_year_names) -> str:
+def main(input) -> str:
     '''This function takes a dictionary as input and calls chinese_zodiac
     and returns the name of the year as a string.'''
-    year = int(input('Enter the year: '))
-    name = chinese_year_names[(year - 3) % 12]
-    return name
+    year = input['year'] # Gets the year from the dictionary.
+    name = chinese_zodiac(year) # Gets the name of the year.
+    # return name as a dictionary:
+    return {'year':year, 'name':name} # Returns the name of the year.
+
+# Run the program:
+if __name__ == "__main__":
+    # test the random_line function:
+    print(random_line())
